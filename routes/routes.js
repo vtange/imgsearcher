1d070cb82e0b73e872bc07b02389aa859340d5dd
Victor Tang <victortang1992@gmail.com>
2016-01-19 20:10:59 -0800

    use Q to chain async. respond in searchBing function

--- routes/routes.js
+++ routes/routes.js
@@ -1,14 +1,19 @@
+var Q = require('q');
+
 // routes.js
 module.exports = function(app) {
 	
-function searchBing(query) {
+function searchBing(query, res) {
     var request=require('request');
     var url="https://api.datamarket.azure.com/Bing/Search/v1/Image?Query='"+query+"'&$format=JSON";
     var key="gpfMNHL5VVhAnl0FaDb90ykr8rMRNYUgYs6f/uK9xiU";
+	var deferred = Q.defer();
     request.get(url, {auth: { user: key, password: key} }, function (error, result) {
-        console.log(error, result.body);
-		return result.body;
+		deferred.resolve(result.body);
     })
+	deferred.promise.then(function (value) {
+		res.send(value);
+	});
 }
 	
     // =====================================
@@ -22,10 +27,10 @@ function searchBing(query) {
 			"params": req.params,
 			"query": req.query
 		};
+		var dude = "dude";
 		//do img search API
 		//store search keywords, drop 11th if 10+
-		result = searchBing(req.params.keywords)
-		res.send(JSON.stringify(result))
+		result = searchBing(req.params.keywords, res)
 	});
 	app.get('/api/latest/imagesearch/:keywords', function(req, res){
 		var result = {};
