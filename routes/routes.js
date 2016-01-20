061cb39f5115a100229edb1c8a9b77b523e7ba2c
Victor Tang <victortang1992@gmail.com>
2016-01-19 22:17:53 -0800

    pagination via API

--- routes/routes.js
+++ routes/routes.js
@@ -5,7 +5,7 @@ module.exports = function(app) {
 	
 function searchBing(query, page, res) {
     var request=require('request');
-    var url="https://api.datamarket.azure.com/Bing/Search/v1/Image?Query='"+query+"'&$format=JSON";
+    var url="https://api.datamarket.azure.com/Bing/Search/v1/Image?Query='"+query+"'&$format=JSON&$skip="+(page*10);
     var key="gpfMNHL5VVhAnl0FaDb90ykr8rMRNYUgYs6f/uK9xiU";
 	var deferred = Q.defer();
     request.get(url, {auth: { user: key, password: key} }, function (error, result) {
@@ -14,13 +14,13 @@ function searchBing(query, page, res) {
 	deferred.promise.then(function (value) {
 		value = JSON.parse(value);
 		var arr = value.d.results;
-		arr = chunk(arr,page);
+		arr = chunk(arr,10);
 		res.send(arr);
 	});
 }
-function chunk(arr, page) {
+function chunk(arr, limit) {
   var newarr = []
-  for (var i = (page-1)*10; i < ((page-1)*10)+10; i++){
+  for (var i = 0; i < limit; i++){
 	   newarr.push(arr[i]);
   }
   return newarr;
@@ -43,7 +43,7 @@ function chunk(arr, page) {
 		result = searchBing(req.params.keywords, req.query.page, res)
 		app.lastSearch.push(req.params.keywords)
 	});
-	app.get('/api/latest/imagesearch/:keywords', function(req, res){
+	app.get('/api/latest/imagesearch', function(req, res){
 		var result = {};
 		//return search keywords
 		
