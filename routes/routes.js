7e57dcb87c8adbac27849aea853a65161978ea6b
Victor Tang <victortang1992@gmail.com>
2016-01-19 16:40:15 -0800

    add request. use request to search bing

--- routes/routes.js
+++ routes/routes.js
@@ -1,6 +1,16 @@
 // routes.js
 module.exports = function(app) {
-
+	
+function searchBing(query) {
+    var request=require('request');
+    var url="https://api.datamarket.azure.com/Bing/Search/v1/Image?Query='"+query+"'&$format=JSON";
+    var key="gpfMNHL5VVhAnl0FaDb90ykr8rMRNYUgYs6f/uK9xiU";
+    request.get(url, {auth: { user: key, password: key} }, function (error, result) {
+        console.log(error, result.body);
+		return result.body;
+    })
+}
+	
     // =====================================
     // HOME PAGE (with login links) ========
     // =====================================
@@ -14,7 +24,7 @@ module.exports = function(app) {
 		};
 		//do img search API
 		//store search keywords, drop 11th if 10+
-		
+		result = searchBing(req.params.keywords)
 		res.send(JSON.stringify(result))
 	});
 	app.get('/api/latest/imagesearch/:keywords', function(req, res){
